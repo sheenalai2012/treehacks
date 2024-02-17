1035931b568342b4897fc87213533dec7480e752
Sheena Lai <sheena.lai@scale.com>
2024-02-17 11:40:38 -0800

    Update firebase.utils.js

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -15,15 +15,25 @@ import {
     setDoc,
 } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-};
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+// };
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCcohjeaTZ0bECAGXWy0q5NlOUUi_VNrgU",
+    authDomain: "treehacks-910bd.firebaseapp.com",
+    projectId: "treehacks-910bd",
+    storageBucket: "treehacks-910bd.appspot.com",
+    messagingSenderId: "755548902592",
+    appId: "1:755548902592:web:c0c060c6920dcc5bf88925",
+    measurementId: "G-3ZLGN94K5E"
+  };
+  
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
